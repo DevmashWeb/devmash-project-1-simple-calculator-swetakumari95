6e157840a5a94f28c7115d6d447e80ec6916167a
Sweta Kumari <sweta191995@gmail.com>
2016-09-30 01:14:11 +0530

    Optimized js code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var out = document.getElementById("output");
 var flag=0; //to clear screen after =
 
-function clicked1(){
+function On_Off(){
 
     //key OFF
     if (flag==1)
@@ -31,226 +31,27 @@ function clicked1(){
     
 }
 
-function clicked2(){
-
-    //key AC
+function clicked(inp){
+    
     if (flag==1)
     {
         out.value = "";
         flag=0;
     }
-    //clears screen value
-    out.value = "";
     
-}
-
-function clicked3(){
-
-    //key CE
-    if (flag==1)
-    {
+    if (inp == 'AC'){
         out.value = "";
-        flag=0;
     }
-    //clears screen value
-    var str = out.value;
-    out.value = str.substring(0,str.length-1);
+    else if (inp == 'CE'){
+        var str = out.value;
+        out.value = str.substring(0, str.length-1);
+    }
+    else
+        out.value += inp;
     
 }
 
-function clicked4(){
-
-    //key /
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "/";
-    
-}
-
-function clicked5(){
-    
-    //key 7
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "7";
-    
-}
-
-function clicked6(){
-    
-    //key 8
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "8";
-    
-}
-
-function clicked7(){
-    
-    //key 9
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "9";
-    
-}
-
-function clicked8(){
-    
-    //key *
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "*";
-    
-}
-
-function clicked9(){
-
-    //key 4
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "4";
-    
-}
-
-function clicked10(){
-    
-    //key 5
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "5";
-    
-}
-
-function clicked11(){
-
-    //key 6
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "6";
-    
-}
-
-function clicked12(){
-    
-    //key -
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "-";
-    
-}
-
-function clicked13(){
-
-    //key 1
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "1";
-    
-}
-
-function clicked14(){
-    
-    //key 2
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "2";
-    
-}
-
-function clicked15(){
-
-    //key 3
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "3";
-    
-}
-
-function clicked16(){
-
-    //key %
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "%";
-    
-}
-
-function clicked17(){
-    
-    //key 0
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "0";
-    
-}
-
-function clicked18(){
-    
-    //key .
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += ".";
-    
-}
-
-function clicked19(){
-    
-    //key +
-    if (flag==1)
-    {
-        out.value = "";
-        flag=0;
-    }
-    out.value += "+";
-    
-}
-
-function clicked20(){
+function calculate(){
     
     //key =
     var exp = out.value;
